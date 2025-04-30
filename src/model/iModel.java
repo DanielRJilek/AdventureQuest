dce2fa81cf7c1e8fbfc6662e931dc694b8cbcdf6
Daniel <jilek.d@northeastern.edu>
2025-04-29 19:37:01 -0500

    Basic methods for model interface added

--- src/model/iModel.java
+++ src/model/iModel.java
@@ -1,4 +1,16 @@
 package model;
 
 public interface iModel {
+  public void move(String s);
+
+  public void drop(String s);
+
+  public void take(String s);
+
+  public void look();
+
+  public void examine(String s);
+  
+  public void use(String s);
+
 }
